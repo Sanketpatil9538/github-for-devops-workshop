37281e5f65f2d36ded6e8df16e9d3cd5161bec97
Sanket <patilsanket792@gmail.com>
2025-05-19 15:55:57 +0530

    First java program

---
+++ Sanket_git/src/sanket_java/JavaFirstProg.java
@@ -0,0 +1,10 @@
+package sanket_java;
+
+public class JavaFirstProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+              System.out.println("Hello welcome to git and git hub");
+	}
+
+}
