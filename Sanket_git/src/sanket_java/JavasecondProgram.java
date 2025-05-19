4a65118e86f5d756a2e91901ffbb02d4e7f1be53
Sanket patil <108356675+Sanketpatil9538@users.noreply.github.com>
2025-05-19 17:20:39 +0530

    Update JavasecondProgram.java

--- Sanket_git/src/sanket_java/JavasecondProgram.java
+++ Sanket_git/src/sanket_java/JavasecondProgram.java
@@ -5,7 +5,10 @@ public class JavasecondProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("Welcome to 2nd program");
-          
+     System.out.println("Welcome to 2nd program ---- new changes were added in github");
+
+   
+		
           
 	}
 
