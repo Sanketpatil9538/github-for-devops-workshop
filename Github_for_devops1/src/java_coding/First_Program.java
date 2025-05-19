1a7ded5f977d3e83dd1f3ac69e1730806f82d627
Sanket <patilsanket792@gmail.com>
2025-05-19 15:30:30 +0530

    Java First Program

---
+++ Github_for_devops1/src/java_coding/First_Program.java
@@ -0,0 +1,10 @@
+package java_coding;
+
+public class First_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Welcome to git and github ");
+	}
+
+}
