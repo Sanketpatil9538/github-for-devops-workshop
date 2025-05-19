e3a33074c0040eb4ffc87a40ae625db50158adcc
Sanket <patilsanket792@gmail.com>
2025-05-19 17:12:28 +0530

    Added new commands

--- Sanket_git/src/sanket_java/JavaFirstProg.java
+++ Sanket_git/src/sanket_java/JavaFirstProg.java
@@ -5,6 +5,10 @@ public class JavaFirstProg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
               System.out.println("Hello welcome to git and git hub");
+              
+              int a= 123;
+              int b=321;
+              System.out.println("The value of a and b is "+ (a+b));
 	}
 
 }
