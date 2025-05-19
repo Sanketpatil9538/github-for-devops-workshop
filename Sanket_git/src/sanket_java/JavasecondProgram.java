6cbddfc26058f0be8930548a40a6d0a4c0606ace
Sanket <patilsanket792@gmail.com>
2025-05-19 17:17:43 +0530

    Adding 2nd program

---
+++ Sanket_git/src/sanket_java/JavasecondProgram.java
@@ -0,0 +1,12 @@
+package sanket_java;
+
+public class JavasecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Welcome to 2nd program");
+          
+          
+	}
+
+}
